4495dba0ba3c7415a3bf653dbab7c07cfa8977f8
Jarek Stachowski <stachowski.jarek@wp.pl>
2019-03-04 12:03:11 +0100

    Initial commit

---
+++ src/main/java/pl/stachowski/AccountingProgram/AccountingProgramApplication.java
@@ -0,0 +1,13 @@
+package pl.stachowski.AccountingProgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingProgramApplication.class, args);
+	}
+
+}
